247be3d71d3fa1898e54efd456650ba5a1a9fe08
Betty22 <erinsiezar@gmail.com>
2020-10-29 01:57:53 -0600

    Cambios en página de historia

--- src/components/Historia.js
+++ src/components/Historia.js
@@ -1,27 +1,40 @@
 import React, { Component } from "react";
+import './Historia.css';
 
  
 class Historia extends Component {
   render() {
     return (
+      <div className="items">
         <div >
-          <h1 className="title">Nuestra historia</h1>
-          <ul >
-            <div className="container" >
-                <img className="image" src="/images/conformacion.jpg"/> 
-                <p1 className="body">
-                    Comprar un accesorio no debe tener que ver con el dinero, sino con un diseño único, materiales de calidad y una apariencia atractiva en general. <br/>
+          <ul>
+            <div>
+              <h1 className="h1">Nuestra historia</h1>
+            </div>
 
-                    Soy Jonah y me encantan los relojes. Incluso soy un fabricante de relojes; Soy dueño de mi propia tienda y puedo adaptar los relojes al estilo único de las personas todos los días. En mi experiencia como minorista de relojes, los precios de los relojes son dramáticamente injustos y los consumidores tienden a comprar relojes basándose en la apariencia de calidad y precio, en lugar de materiales y diseño de alta calidad. Por eso me propuse seleccionar una selección única de relojes de todo el mundo que están construidos con materiales de la más alta calidad, con un diseño atemporal, pero a una fracción del costo de un reloj de marca.
-
-                    Obtenemos nuestros relojes de varios proveedores en los EE. UU., Europa y Asia, lo que nos permite ofrecer precios al por mayor, con una variedad que a menudo cambia con materiales y diseño de la más alta calidad. Debido a que enviamos desde múltiples almacenes, espere hasta 2-3 semanas para cualquier artículo que pueda enviarse desde almacenes internacionales.
-
-                    Si tiene alguna pregunta o comentario, ¡nos encantaría escucharlo!
-                </p1>
+            <div > 
+              <img  className="image-h" src="/images/conformacion.jpg" alt="autor" width="350" height="450" />
+            </div>  
+          </ul>
+        </div>  
+        <div className="parrafo-h"> 
+          <ul>
+            <div >
+              <p>
+              Comprar un accesorio no debe tener que ver con el dinero, sino con un diseño único, 
+              materiales de calidad y una apariencia atractiva en general. <br/><br/>
+              Somos Kimberly Herrera y Rosario Jinestay, nos encantan los accesorios. Somos dueñas de nuestra 
+              propia propia tienda virtual y podemos adaptar los accesorios al estilo único de las personas 
+              de todos los días. <br/><br/>Obtenemos nuestros productos de varios proveedores en los EE. UU., 
+              Europa y Asia, lo que nos permite ofrecer precios con una variedad que a menudo cambia con 
+              materiales y diseño de la más alta calidad. 
+              <br/><br/>
+              Si tiene alguna pregunta o comentario, ¡nos encantaría escucharlo!
+              </p>
             </div>
           </ul>
-          
         </div>
+ </div>
     );
   }
 }
