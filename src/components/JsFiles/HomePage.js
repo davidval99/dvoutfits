cfa8d999fb90c9dd22744df58a75b975cabb598a
danblanco <blancodaniel2010@hotmail.com>
2020-11-20 15:34:50 -0600

    component for products preview is dinamic

--- src/components/JsFiles/HomePage.js
+++ src/components/JsFiles/HomePage.js
@@ -3,6 +3,7 @@ import '../CssFiles/HomePage.css';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import Home from '../Home/Home';
 import CarrouselComponent from './CarrouselComponent';
+import ProductPreview from "./productPreview";
 
 
 function HomePage(){
@@ -10,70 +11,7 @@ function HomePage(){
         
         <BrowserRouter>
         < CarrouselComponent />
-
-       
-        <ul className="products">
-            <li>
-            <div className="product">
-                <img className="product-image" src="/images/d1.jpg" alt="product" />
-                <div className="product-name">
-                    <a href="product.html">Pulsera</a>
-                </div>
-                <div className="product-brand">Pulsera de cristales checos y dijes de ojo turco </div>
-                <div className="product-price">$10</div>
-                </div>
-            </li>
-            <li>
-            <div className="product">
-                <img className="product-image" src="/images/d2.jpg" alt="product" />
-                <div className="product-name">
-                    <a href="product.html">Pulsera</a>
-                </div>
-                <div className="product-brand">Pulsera de cristales checos y dijes de ojo turco </div>
-                <div className="product-price">$10</div>
-                </div>
-            </li>
-            <li>
-            <div className="product">
-                <img className="product-image" src="/images/d3.jpg" alt="product" />
-                <div className="product-name">
-                <a href="product.html">Pulsera</a>
-                </div>
-                <div className="product-brand">Pulsera de cristales checos y dijes de ojo turco </div>
-                <div className="product-price">$10</div>
-            </div>
-            </li>
-            <li>
-            <div className="product">
-                <img className="product-image" src="/images/p1.jpg" alt="product" />
-                <div className="product-name">
-                <a href="product.html">Pulsera</a>
-                </div>
-                <div className="product-brand">Pulsera de cristales checos y dijes de ojo turco </div>
-                <div className="product-price">$10</div>
-            </div>
-            </li>
-            <li>
-            <div className="product">
-                <img className="product-image" src="/images/p2.jpg" alt="product" />
-                <div className="product-name">
-                <a href="product.html">Pulsera</a>
-                </div>
-                <div className="product-brand">Pulsera de cristales checos y dijes de ojo turco </div>
-                <div className="product-price">$10</div>
-            </div>
-            </li>
-            <li>
-            <div className="product">
-                <img className="product-image" src="/images/p3.jpg" alt="product" />
-                <div className="product-name">
-                <a href="product.html">Pulsera</a>
-                </div>
-                <div className="product-brand">Pulsera de cristales checos y dijes de ojo turco </div>
-                <div className="product-price">$10</div>
-            </div>
-            </li>
-        </ul>
+        < ProductPreview  />
         </BrowserRouter>
 
     )
